75b98384443e4a3776b9912b2b26b04ee282dfdd
Jure Majnik <majnik.jure@gmail.com>
2024-04-11 18:10:07 +0200

    first commit of new stepper hardware

--- pico_stepper_whisker_stim/blink.py
+++ pico_stepper_whisker_stim/blink.py
@@ -1,1 +1,32 @@
 # first testing the pins if they work or not
+
+from machine import Pin
+import time
+
+# define the pins 12, 13, 14, 15
+p12 = Pin(12, Pin.OUT)
+p13 = Pin(13, Pin.OUT)
+p14 = Pin(14, Pin.OUT)
+p15 = Pin(15, Pin.OUT)
+
+# blink the pins
+for i in range(10):
+    p12.value(1)
+    time.sleep(0.5)
+    p12.value(0)
+    time.sleep(0.5)
+
+    p13.value(1)
+    time.sleep(0.5)
+    p13.value(0)
+    time.sleep(0.5)
+
+    p14.value(1)
+    time.sleep(0.5)
+    p14.value(0)
+    time.sleep(0.5)
+
+    p15.value(1)
+    time.sleep(0.5)
+    p15.value(0)
+    time.sleep(0.5)
